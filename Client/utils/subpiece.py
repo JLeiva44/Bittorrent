21ab2e021850272ff2dd393b18a16f720a5144b5
jose_leiva <jseleivaf686@gmail.com>
2024-06-28 04:00:37 +0200

    start subpice_file

---
+++ Client/utils/subpiece.py
@@ -0,0 +1,20 @@
+from enum import Enum
+
+SUBPIECE_SIZE = 16383 # 16kb = 16383 bits = 2^14 bits)
+
+class State(Enum):
+    FREE = 0
+    PENDING = 1
+    FULL = 2
+
+class SubPiece:
+    def __init__(self, state : State = State.FREE, subpiece_size: int = SUBPIECE_SIZE, data:bytes = b'', last_seen:float = 0 ) -> None:
+        self.state:State = state
+        self.subpiece_size:int = subpiece_size
+        self.data: bytes = data
+        self.last_seen:float = last_seen
+
+    def __str__(self):
+
+        return "%s - %d - %d - %d" % (self.state, self.subpiece_size, len(self.data), self.last_seen)    
+
