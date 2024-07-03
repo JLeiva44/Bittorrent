4482258ed034628287c369f52f7f457e16e56e4e
jose_leiva <jseleivaf686@gmail.com>
2024-07-03 06:41:44 +0200

    made some changes in Torrent class

--- Client/torrent/torrent.py
+++ Client/torrent/torrent.py
@@ -1,98 +1,88 @@
 from hashlib import sha1
-from hashlib import md5
-import bencodepy
-import datetime
-import os
+from . import bencoding
+from collections import namedtuple
 
-class TorrentCreator:
-    def __init__(self,file_path, piece_size, private, announce_list, comments, source) -> None:
-        self.file_path = file_path
-        self.piece_size = piece_size
-        self.announce_list = announce_list
-        self.comments = comments
-        self.private = private
-        self.source = source
-        self.file_size = os.path.getsize(self.file_path)
-        self.md5sum = md5(open(self.file_path,'rb').read()).digest()
-        self.file_name = os.path.basename(self.file_path)
-
-    @property 
-    def hash_pieces():
-        pass 
-
-    def make_metainfo(self):
+class TorrentMaker:
+    def __init__(self) -> None:
         pass
 
-    def make_torrentfile(self):
-        pass   
-
-   
-        
-
+TorrentFile = namedtuple('File',['name','length'])
 class Torrent :
-    """ A class that represents a Torrent file data"""
+    """Represents the torrent meta-data that is kept within a .torrent file"""
 
-    def __init__(self, torrentfile_path) -> None:
-        self.torrentfile_path = torrentfile_path
-        self.metadata = TorrentMetadata(self._parse_torrentfile())
-        self.info = self.metadata.info
+    def __init__(self, file_path) -> None:
+        self.file_path = file_path
         self.files = []
 
-    def _parse_torrentfile(self):
-        t_file = open(self.torrentfile_path,'rb')
-        content = t_file.read()    
-        metainfo = bencodepy.decode(content)
-        t_file.close()
-        return metainfo
+        with open(self.torrentfile_path, 'rb') as f:
+            meta_info = f.read()
+            self.meta_info = bencoding.Decoder(meta_info).decode()
+            info = bencoding.Encoder(self.meta_info[b'info']).encode()
+            self.info_hash = sha1(info).digest()
+            self._identify_files()
+
+    def _identify_files(self):
+        """
+        Identifies the files included in this torrent
+        """
+        self.files.append(
+            TorrentFile(
+                self.meta_info[b'info'][b'name'].decode('utf-8'),
+                self.meta_info[b'info'][b'length'])
+        )
+
+    @property
+    def announce(self):
+        """
+        The announce URL to the tracker
+        """
+        return self.meta_info[b'announce'].decode('utf-8')
+    
+    @property 
+    def piece_length(self):
+        """
+        Get the length in bytes for each piece
+        """
+        return self.meta_info[b'info'][b'piece length']
+
+    @property 
+    def total_size(self):
+        """
+        The total size (in bytes) for all the files in this torrent. For a
+        single file torrent this is the only file, for a multi-file torrent
+        this is the sum of all files.
+
+        :return: The total size (in bytes) for this torrent's data.
+        """
+        return self.files[0].length
     
     @property
-    def announce(self) -> str:
+    def pieces(self):
         """
-        The announce URLs of the tracker(s)
+        The info pieces is a string representing all pieces SHA1 hashes
+        (ecah 20 bytes long).read that data and slice it up into the actual pieces
         """
-        return self.metadata[b'announce']
-    
+        data = self.meta_info[b'info'][b'pieces']
+        pieces = []
+        offset = 0
+        length = len(data) 
+
+        while offset < length:
+            pieces.append(data[offset:offset+20])
+            offset += 20
+        return pieces 
+
     @property
-    def announce_list(self)-> list:
-        trackers = []
-        for tracker in self.metadata[b'announce-list']:
-            splited = tracker.split(b':')
-            trackers.append((splited[0], int(splited[1])))
-        return trackers    
-    
-    # def __str__(self):
-    #     return 'Filename: {0}\n' \
-    #            'File length: {1}\n' \
-    #            'Announce URL: {2}\n' \
-    #            'Hash: {3}'.format(self.meta_info[b'info'][b'name'],
-    #                               self.meta_info[b'info'][b'length'],
-    #                               self.meta_info[b'announce'],
-    #                               self.info_hash)
-    
+    def output_file(self):
+        return self.meta_info[b'info'][b'name'].decode('utf-8')
 
-t = Torrent("/media/jose/A63C16883C1654211/Proyectos/Bittorrent/Bittorrent/Client/torrent/archivo.torrent")
-print(t.metadata)    
-
-class TorrentMetadata:
-    def __init__(self, metadata) -> None:
-        self.announce = metadata[b'announce'] # URL del tracker
-        self.announce_list = metadata[b'announce-list']
-        self.comment = metadata[b'comment']
-        self.created_by = metadata[b'created by']
-        self.creation_date = metadata[b'creation date']
-        self.info = TorrentInfo(metadata[b'info'])
-        
-
-class TorrentInfo:   
-    def __init__(self, info) -> None:
-        self.length = info[b'length'] # tamano del archivo en bytes
-        self.md5sum = info[b'md5sum']
-        self.name = info[b'name']
-        self.piece_length = info[b'piece length'] # numero de bytes x pieza
-        self.pieces = info[b'pieces'] # una lista de hash. Concatenacion de cada hash SHA1 de las piezas
-        self.private = info[b'private']
-        self.info_hash = sha1(bencodepy.encode(info)).hexdigest() 
+    def __str__(self):
+        return 'Filename: {0}\n' \
+               'File length: {1}\n' \
+               'Announce URL: {2}\n' \
+               'Hash: {3}'.format(self.meta_info[b'info'][b'name'],
+                                  self.meta_info[b'info'][b'length'],
+                                  self.meta_info[b'announce'],
+                                  self.info_hash)
 
 
-p = TorrentMetadata(t.metadata)
-print(p.announce)        
