ef8ff2e8e7bab969bb535dfaa85c0a90affc8f6f
jose_leiva <jseleivaf686@gmail.com>
2024-07-06 04:53:00 +0200

    reordering

--- Client/client.py
+++ Client/client.py
@@ -1,17 +1,28 @@
+from torrent.torrent import TorrentMaker
+
+
+PIECE_SIZE =  2**18 # 256 Kb (kilobibits) = 262144 bits
+
 class Client:
     def __init__(self, client_id, ip, port) -> None:
         self.id = client_id
         self.ip = ip
         self.port = port
 
-    def upload_file(self, file):
-        pass
+    def seed_file(self, file_path, tracker_url): # Ver despues si poner mas parametros como PRIVATE, COMMents, SOURCE ...
+        torrent_maker = TorrentMaker(file_path,PIECE_SIZE, tracker_url, 'pepe')
+        pieces = torrent_maker.pieces
+
+        tracker_ip, tracker_port = tracker_url.split(':')
+        print(pieces)
+        print(tracker_ip, tracker_port)
+         
 
     def download_file(self,file):
         pass
     
-    def create_torrent(self, **kwargs):
-        pass
 
         
 
+c = Client(1,'127.222.333.11','1880')
+c.seed_file('Files/music1.txt','127.222.11.11:1080')
