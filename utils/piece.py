8ce4240aedec60a5b85d2de72ccf0fb476127f0b
joseLeiva <jseleivaf686@gmail.com>
2024-11-19 12:49:07 -0500

    update utils

--- utils/piece.py
+++ utils/piece.py
@@ -3,70 +3,56 @@ import math
 import time 
 import logging
 #import zmq
+from utils.disk_io import DiskIO
 
-from subpiece import SubPiece, SUBPIECE_SIZE, State
+from subpiece import SubPiece, DEFAULT_SUBPIECE_SIZE, State
 
 class Piece:
     def __init__(self, piece_index:int, piece_offset, piece_size:int, piece_hash :str) -> None:
-        self.piece_index:int = piece_index
+        
+        self.piece_index = piece_index
         self.piece_offset = piece_offset
-        self.piece_size:int = piece_size
-        self.piece_hash :str = piece_hash
-        self.is_full: bool = False
+        self.piece_size = piece_size
+        self.piece_hash = piece_hash
+        self.is_full = False
         self.files = []
         self.raw_data:bytes = b''
-        self.number_of_subpieces:int = int(math.ceil(float(piece_size) / SUBPIECE_SIZE))
-        self.subpieces : list[SubPiece] = []
-
-        self._init_subpieces()
+        self.number_of_subpieces:int = int(math.ceil(float(piece_size) / DEFAULT_SUBPIECE_SIZE))
+        self.subpieces : list[SubPiece] = self._init_subpieces()
 
 
-    def _init_subpieces(self): # see this
-        
-        #if self.number_of_subpieces > 1:
-        for i in range(self.number_of_subpieces -1):
-            self.subpieces.append(SubPiece(subpiece_size=SUBPIECE_SIZE))
+    @property
+    def in_memory(self):
+        return self.raw_data != b''  
+    
+    def put_data(self, data):
+        self.raw_data = data
+        self.is_full = True
+    
+    @property
+    def have_all_subpieces(self):
+        '''
+            If all subpieces of the piece succefully downloaded
+        '''
+        return all(sub.state == State.FULL for sub in self.subpieces) 
 
-            # Last subpiece of last piece, the special block
-        #if self.piece_size % SUBPIECE_SIZE > 0: # la ultima tiene el tamano que alcance
-        self.subpieces.append(SubPiece(subpiece_size= self.piece_size % SUBPIECE_SIZE))
-
-        # else: # 1 solo bloque => la pieza y la subpieza son del mismo tamano
-        #     self.subpieces.append(SubPiece(subpiece_size=int(self.piece_size)))            
-
-    def get_empty_subpiece(self):
-        if self.is_full:
-            return None
-        
-        for index,subpiece in enumerate(self.subpieces):
-            if subpiece.state == State.FREE:
-                self.subpieces[index].state = State.PENDING 
-                self.subpieces[index].last_seen = time.time()
-                return self.piece_index, index*SUBPIECE_SIZE, subpiece.subpiece_size
-            
-        return None
-
-    def all_subpieces_full(self):
-        for subpiece in self.subpieces:
-            if subpiece.state == State.FREE or subpiece.state == State.PENDING:
-                return False
-        return True
-
-    def get_subpiece(self, subpiece_offset):
-        subpiece_index = subpiece_offset // SUBPIECE_SIZE
-        return self.subpieces[subpiece_index]
-
-    def set_subpiece(self, offset, data):
-        index = int(offset/SUBPIECE_SIZE)
+    def write_subpiece(self, offset, data):
+        index = offset// DEFAULT_SUBPIECE_SIZE
 
         if not self.is_full and not self.subpieces[index].state == State.FULL:
             self.subpieces[index].data = data
-            self.subpieces[index].state = State.FULL        
+            self.subpieces[index].state = State.FULL
 
-    def update_subpiece_status(self): # if block is pending for too long: set it free
-        for i, subpiece in enumerate(self.subpieces):
-            if subpiece.state == State.PENDING and (time.time() - subpiece.last_seen)>5:
-                self.subpieces[i] = SubPiece()
+        if self.have_all_subpieces:
+            self._merge_all_subpieces()  
+    
+    def _init_subpieces(self): # see this
+        result  = []
+        for _ in range(self.number_of_subpieces -1):
+            result.append(SubPiece(subpiece_size=DEFAULT_SUBPIECE_SIZE))
+
+        result.append(SubPiece(subpiece_size= self.piece_size % DEFAULT_SUBPIECE_SIZE))
+        return result
 
     def _merge_subpieces(self):
         data = b''
@@ -74,11 +60,10 @@ class Piece:
         for subpiece in self.subpieces:
             data += subpiece.data
 
-        return data     
-
+        return data   
 
     def _valid_subpieces(self,raw_data):
-        hashed_piece_raw_data = hashlib.shal(raw_data).digest()
+        hashed_piece_raw_data = hashlib.sha1(raw_data).digest() # hexdigest??
         if hashed_piece_raw_data == self.piece_hash:
             return True
 
@@ -86,72 +71,75 @@ class Piece:
         # logging.debug("{} : {}".format(hashed_piece_raw_data, self.piece_hash))
         return False
     
-    @property
-    def have_all_blocks(self):
-        '''
-            If all block of the piece succefully downloaded
-        '''
-        return all(sub.state == State.FULL for sub in self.subpieces) 
-    
-    def clean_memory(self):
-        self.raw_data = b''
-
-    def put_data(self, data):
-        self.raw_data = data
-        self.is_full = True
-
-    @property
-    def in_memory(self):
-        return self.raw_data != b''  
-
-    def load_from_disk(self, filename : str):
-
-        # filename, self.piece_ofsset, self.piece_size
-        new_file = open(filename, 'rb')
-        new_file.seek(self.piece_offset)
-        raw_data = new_file.read(self.piece_size)
-        new_file.close()
-        piece_data = raw_data      
-
-        self.raw_data = piece_data
-        self._rebuild_subpieces()
+    def _merge_all_subpieces(self):
+        raw_data = self._merge_subpieces()
+        if self._valid_subpieces(raw_data):
+            self.is_full = True
+            self.raw_data = raw_data
+            # logger. debug(....)
+        else:
+            self.subpieces = self._init_subpieces()
 
     def _rebuild_subpieces(self):
         for i in range(self.number_of_subpieces -1):
-            self.subpieces[i].data = self.raw_data[i * SUBPIECE_SIZE: (i+1)* SUBPIECE_SIZE]
-        self.subpieces[self.number_of_subpieces - 1].data = self. raw_data[(self.number_of_subpieces-1)* SUBPIECE_SIZE]    
+            self.subpieces[i].data = self.raw_data[i * DEFAULT_SUBPIECE_SIZE: (i+1)* DEFAULT_SUBPIECE_SIZE]
+        self.subpieces[self.number_of_subpieces - 1].data = self. raw_data[(self.number_of_subpieces-1)* DEFAULT_SUBPIECE_SIZE]    
+        
+    def get_subpiece(self, subpiece_offset):
+        subpiece_index = subpiece_offset // DEFAULT_SUBPIECE_SIZE
+        return self.subpieces[subpiece_index]
+    
+    def load_from_disk(self, filename : str):
 
+        piece_data = DiskIO.read_from_disk(filename, self.piece_offset, self.piece_size)
+        self.raw_data = piece_data
+        self._rebuild_subpieces()
+
+    def clean_memory(self):
+        self.raw_data = b''    
+
+    def get_empty_subpiece(self):
+        if self.is_completed:
+            return None
+        
+        for index, block in enumerate(self.subpieces):
+            if block.state == State.FREE:
+                self.subpieces[index].state = State.PENDING
+                return index * DEFAULT_SUBPIECE_SIZE, self.subpieces[index].subpiece_size
+        
+        return None     
+    
+    def all_subpieces_full(self):
+        for subpiece in self.subpieces:
+            if subpiece.state == State.FREE or subpiece.state == State.PENDING:
+                return False
+        return True
+
+
+    # def set_subpiece(self, offset, data):
+    #     index = int(offset/DEFAULT_SUBPIECE_SIZE)
+
+    #     if not self.is_full and not self.subpieces[index].state == State.FULL:
+    #         self.subpieces[index].data = data
+    #         self.subpieces[index].state = State.FULL        
+
+    def update_subpiece_status(self): # if block is pending for too long: set it free
+        for i, subpiece in enumerate(self.subpieces):
+            if subpiece.state == State.PENDING and (time.time() - subpiece.last_seen)>5:
+                self.subpieces[i] = SubPiece()
+
+   
+   
 
     
-    def write_subpiece(self, offset, data):
-        index = offset// SUBPIECE_SIZE
+    
+     
+   
 
-        if not self.is_full and not self.subpieces[index].state == State.FULL:
-            self.subpieces[index].data = data
-            self.subpieces[index].state = State.FULL
+    
+        
+   
 
-        if self.have_all_blocks:
-            self._merge_subpieces()    
-
-    def _wite_piece_on_disk(self):
-        for file in self.files:
-            file_path = file["path"]
-            file_offset = file["fileOffset"]
-            piece_offset = file["pieceOffset"]
-            length = file["length"]
-
-            try:
-                f = open(file_path, 'r+b') # Already existing file
-            except IOError:
-                f = open(file_path, 'wb') # new file
-            except Exception:
-                logging.exception("Can't write to file")
-                return 
-
-            f.seek(file_offset)
-            f.write(self.raw_data[piece_offset:piece_offset+length])
-            f.close()
-                        
 
 print("sdkjbfdjbf")
 
